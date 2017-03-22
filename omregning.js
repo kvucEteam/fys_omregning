0daeb9f44fcc7e3cf983f6cb890f01725c579d8a
Anders Tornøe <ato@kvuc.dk>
2017-03-22 10:53:11 +0100

    no message

--- omregning.js
+++ omregning.js
@@ -2,15 +2,18 @@ var korrektsvar;
 var feedback_Array = [];
 var feedback_counter = 0;
 
+var storrelse_Array = [];
+
+var korrektfeed = ["Rigtigt svaret", "Savret er korrekt", "Flot, svaret er rigtigt", "Du har svaret rigtigt", "Det er det rigtige svar", "Helt rigtigt"]
+
 
 
 $(document).ready(function() {
 
+
     $('#instruction').prepend(instruction("Omregn størrelsen fra den ene enhed til den anden. Skriv den rigtige værdi i tekstfeltet."));
     $('#explanation').prepend(explanation("Formålet med denne øvelse er at lære dig at omregne mellem forskellige enheder for den samme fysiske størrelse. Denne type omregninger har man hele tiden brug for i fysikfaget."));
 
-
-    poseQuestion();
     $(".btn-tjek").click(tjek_svar);
 
     $(document).keypress(function(e) {
@@ -19,19 +22,92 @@ $(document).ready(function() {
         }
     });
 
-    $(".inputfield").change(function() {
-        console.log("input change");
-        //$(".microhint").remove();
+
+
+    $(".postfix").click(function() {
+        //$(".postfix").slideToggle();
     });
+
+
+
+
+    build_select_container();
+
+
+    $('input[type="checkbox"]').change(function() {
+        var name = $(this).val();
+        var check = $(this).prop('checked');
+        console.log("Change: " + name + " to " + check);
+        poseQuestion();
+    });
+
+  $('.inputfield').on('input',function(e){
+     $(".microhint").remove()
+    });
+
+
+    //$(".cb_container").slideToggle();
+    $(".cb_container").slideToggle(0);
+
+    poseQuestion();
 
 });
 
+function build_select_container() {
+    var HTML = "<div class='toggle_btn'><span class='toggleglyph glyphicon glyphicon-chevron-down'></span><span class='valg_txt'>Klik for at vælge hvilke fysiske størrelser du vil træne</span></div>";
+    HTML += "<div class='cb_container'>";
+    for (var i = 0; i < jsonData.length; i++) {
+
+        HTML += " <label class='input_p'><input class='checkboxes_str' checked type='checkbox' name='storrelse' value=" + jsonData[i].storrelse + "><span class='checkbox_txt'>" + jsonData[i].storrelse + "</span></label>";
+
+    }
+
+    HTML += "</div>";
+
+    $(".select_container").html(HTML);
+
+    $(".toggle_btn").click(function() {
+        $(".cb_container").toggle(200, function() {
+            if ($(".cb_container").is(":hidden")) {
+                $(".toggleglyph").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
+            } else {
+                $(".toggleglyph").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+            }
+        });
+
+
+    });
+
+
+
+}
+
+function update_checkboxes() {
+    console.log("updating cb");
+}
 
 function poseQuestion() {
+
+    $(".microhint").remove();
+    $(".question, .inputcontainer").fadeOut(0);
+
+
+
+    var randomklasse = Math.floor(Math.random() * jsonData.length);
+    //Hvis det ransom nummer ikke er selected, kør igen:
+
+    if ($(".checkboxes_str:checked").length > 0) {
+        while (!$(".checkboxes_str").eq(randomklasse).prop('checked')) {
+            randomklasse = Math.floor(Math.random() * jsonData.length);
+        }
+    } else {
+
+    }
+
+
     $(".inputfield").val("");
     $(".inputfield").focus();
     var randomvalue;
-    var randomklasse = Math.floor(Math.random() * jsonData.length);
 
     console.log(randomklasse);
 
@@ -77,24 +153,33 @@ function poseQuestion() {
 
     console.log("RVK:  " + randomvalue_komma);
 
+    var faktor_komma = jsonSelected.faktor.toString();
+    faktor_komma = faktor_komma.replace(".", ",");
+
+
+
 
 
 
     $(".kategori").html(jsonData);
 
     if (reverse === 0) {
-        $(".question").html("Omregn venligst " + randomvalue_komma + " " + string_val_0 + " til " + string_val_1);
+        $(".question").html("Omregn " + randomvalue_komma + " " + string_val_0 + " til " + string_val_1);
         korrektsvar = (randomvalue * jsonSelected.faktor);
         $(".first_val").html(randomvalue_komma + " " + jsonSelected.kombo[0] + " = ");
         $(".second_val").html(" " + jsonSelected.kombo[1] + "");
-        feedback_Array = ["Den ene enhed er " + jsonSelected.faktor + " gange større end den anden", "Skal resultatet være større eller mindre, tror du?", "Der går " + jsonSelected.faktor + " " + jsonSelected.kombo[1] + " på 1 " + jsonSelected.kombo[0], "Du skal gange med " + jsonSelected.faktor, randomvalue_komma + " " + jsonSelected.kombo[0] + " gange med " + jsonSelected.faktor + " giver resultatet: <h4>" + korrektsvar + " " + jsonSelected.kombo[1] + "</h4>"];
+        var korrekt_res_komma = korrektsvar.toString();
+        korrekt_res_komma = korrekt_res_komma.replace(".", ",");
+        feedback_Array = ["Den ene enhed er " + faktor_komma + " gange større end den anden", faktor_komma + " " + jsonSelected.kombo[1] + " er det samme som 1 " + jsonSelected.kombo[0], "Overvej om resultatet skal være større eller mindre", "Du skal gange med " + faktor_komma, randomvalue_komma + " " + jsonSelected.kombo[0] + " gange med " + faktor_komma + " giver resultatet: <h4>" + korrekt_res_komma + " " + jsonSelected.kombo[1] + "</h4>"];
 
     } else {
         $(".question").html("Omregn " + randomvalue_komma + " " + string_val_1 + " til " + string_val_0);
         korrektsvar = (randomvalue / jsonSelected.faktor);
         $(".first_val").html(randomvalue_komma + " " + jsonSelected.kombo[1] + " = ");
         $(".second_val").html(" " + jsonSelected.kombo[0] + "");
-        feedback_Array = ["Den ene enhed er " + jsonSelected.faktor + " gange større end den anden", "Skal resultatet være større eller mindre, tror du?", "Der går " + jsonSelected.faktor + " " + jsonSelected.kombo[1] + " på 1 " + jsonSelected.kombo[0], "Du skal dividere med " + jsonSelected.faktor, randomvalue_komma + " " + jsonSelected.kombo[1] + " divideret med " + jsonSelected.faktor + " giver resultatet: <h4>" + korrektsvar + " " + jsonSelected.kombo[0] + "</h4>"];
+        var korrekt_res_komma = korrektsvar.toString();
+        korrekt_res_komma = korrekt_res_komma.replace(".", ",");
+        feedback_Array = ["Den ene enhed er " + faktor_komma + " gange større end den anden", faktor_komma + " " + jsonSelected.kombo[1] + " er det samme som 1 " + jsonSelected.kombo[0], "Overvej om resultatet skal være større eller mindre", "Du skal dividere med " + faktor_komma, randomvalue_komma + " " + jsonSelected.kombo[1] + " divideret med " + faktor_komma + " giver resultatet: <h4>" + korrekt_res_komma + " " + jsonSelected.kombo[0] + "</h4>"];
     }
 
 
@@ -105,29 +190,48 @@ function poseQuestion() {
 
     $(".postfix").html("Korrekt afrundet svar er: " + korrektsvar); // + " Decimaler" + );
 
+    console.log(jsonSelected.faktor + ", " + jsonSelected.reversible + ", " + randomvalue);
 
+    if (jsonSelected.faktor > 1000 && jsonSelected.reversible == true && randomvalue < 1) {
+        poseQuestion();
+        return;
+    }
 
 
     var antal_decimaler = countDecimals(korrektsvar);
-    if (antal_decimaler > 10) {
+    if (antal_decimaler > 8) {
         console.log("KØRER IGEN");
         poseQuestion();
     }
+    $(".question, .inputcontainer").fadeIn(500);
 }
 
 
 function tjek_svar() {
-    var brugersvar = $(".inputfield").val().toString();
-    brugersvar = brugersvar.replace(/,/g, ".")
-    console.log(brugersvar + ", " + korrektsvar);
-    if (brugersvar == korrektsvar) {
-        microhint($(".inputfield"), "<h4>Rigtigt</h4>", "green");
-        poseQuestion();
+
+    if ($(".btn_onward").html() == "Tjek svar") {
+        var brugersvar = $(".inputfield").val().toString();
+        brugersvar = brugersvar.replace(/,/g, ".")
+        console.log(brugersvar + ", " + korrektsvar);
+        if (brugersvar == korrektsvar) {
+            microhint($(".inputfield"), "<p>" + korrektfeed[Math.floor(Math.random() * korrektfeed.length)] + "</p>", "#2ABB2A");
+
+            $(".btn-tjek").html("Næste");
+
+            feedback_counter = 0;
+
+            //poseQuestion();
+
+        } else {
+            microhint($(".inputfield"), "<p>" + feedback_Array[feedback_counter], "#e26060");
+            if (feedback_counter < feedback_Array.length - 1) {
+                feedback_counter++;
+            } else { feedback_counter = 0 }
+        }
     } else {
-        microhint($(".inputfield"), "<h4>Hjælp</h4><p>" + feedback_Array[feedback_counter], "red");
-        if (feedback_counter < feedback_Array.length - 1) {
-            feedback_counter++;
-        } else { feedback_counter = 0 }
+        $(".btn_onward").html("Tjek svar");
+        poseQuestion();
+
     }
 }
 
